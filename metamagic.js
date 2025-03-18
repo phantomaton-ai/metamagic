a30d1a7915d65354b544a29771b64e0edff86e33
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-03-18 14:22:54 -0700

    Updated by Phantomaton

--- metamagic.js
+++ metamagic.js
@@ -1,14 +1,14 @@
-import { processOptions } from './options.js';
-import { createValidator } from './validator.js';
-import { createExample } from './exemplifier.js';
+import processOptions from './options.js';
+import createValidator from './validator.js';
+import createExample from './exemplifier.js';
 
-export default function metamagic(name, execute, options = {}) {
-  const processedOptions = processOptions(name, options);
+export default function metamagic(name, execute, opts = {}) {
+  const processedOptions = processOptions(name, opts);
 
   return {
     name,
     description: processedOptions.description,
-    example: createExample(options.example, name),
+    example: createExample(opts.example, name),
     validate: createValidator(
       processedOptions.attributes, 
       processedOptions.body
