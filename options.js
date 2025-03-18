e5ed0ab94a172cc6d0f0a878a5d0a74613657eb0
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-03-18 14:22:54 -0700

    Updated by Phantomaton

--- options.js
+++ options.js
@@ -1,31 +1,24 @@
-export function normalizeAttributeConfig(config) {
-  if (config === true) return { optional: false };
-  if (config === false) return { optional: true };
-  if (typeof config === 'string') return { description: config, optional: false };
-  return { optional: false, ...config };
-}
+export default function processOptions(name, opts = {}) {
+  const normalizeConfig = (config) => {
+    if (config === true) return { optional: false };
+    if (config === false) return { optional: true };
+    if (typeof config === 'string') return { description: config, optional: false };
+    return { optional: false, ...config };
+  };
 
-export function normalizeBodyConfig(config) {
-  if (config === true) return { optional: false };
-  if (config === false) return { optional: true };
-  if (typeof config === 'string') return { description: config, optional: false };
-  return { optional: false, ...config };
-}
-
-export function processOptions(name, options = {}) {
   return {
-    attributes: options.attributes 
+    attributes: opts.attributes 
       ? Object.fromEntries(
-          Object.entries(options.attributes).map(([key, value]) => 
-            [key, normalizeAttributeConfig(value)]
+          Object.entries(opts.attributes).map(([key, value]) => 
+            [key, normalizeConfig(value)]
           )
         )
       : {},
-    body: options.body 
-      ? normalizeBodyConfig(options.body)
+    body: opts.body 
+      ? normalizeConfig(opts.body)
       : { optional: true },
-    description: options.description || `A command named ${name}`,
-    example: options.example || {
+    description: opts.description || `A command named ${name}`,
+    example: opts.example || {
       attributes: {},
       body: null
     }
